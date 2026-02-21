aab71f5ea3fef193719047251dd9803e561413b2
JackL456 <146298763+JuzzLuzz@users.noreply.github.com>
2026-02-21 10:56:14 -0500

    Creating Indexer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,8 @@ import frc.robot.commands.Autos;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Visulization;
+import frc.robot.subsystems.Indexer.Indexer;
+import frc.robot.subsystems.Indexer.IndexerIOTalon;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.Shooter.ShooterIOSim;
 import frc.robot.subsystems.Turret.Turret;
@@ -59,6 +61,7 @@ public class RobotContainer {
   private static Turret turret;
   private static Shooter shooter;
   private static Intake intake;
+  private static Indexer indexer;
   private static Visulization visulization = null;
 
   //  Command Declaration
@@ -85,6 +88,7 @@ public class RobotContainer {
         intake = new Intake(new IntakeIOTalonFX());
         shooter = new Shooter(null);
         turret = new Turret(null, drivetrain::getPose, drivetrain::getFieldRelativeChassisSpeeds);
+        indexer = new Indexer(new IndexerIOTalon());
         break;
 
       case SIM:
@@ -92,6 +96,7 @@ public class RobotContainer {
         intake = new Intake(new IntakeIOSim());
         shooter = new Shooter(new ShooterIOSim());
         turret = new Turret(new TurretIOSim(), drivetrain::getPose, drivetrain::getFieldRelativeChassisSpeeds);
+        indexer = new Indexer(null);
         visulization = new Visulization(fuelSim, drivetrain::getPose, turret, shooter, intake);
         configureFuelSimRobot(visulization::canIntake, visulization::intakeFuel);
         break;
@@ -100,6 +105,7 @@ public class RobotContainer {
         intake = new Intake(null);
         shooter = new Shooter(null);
         turret = new Turret(null, drivetrain::getPose, drivetrain::getFieldRelativeChassisSpeeds);
+        indexer = new Indexer(null);
         break;
     }
 
