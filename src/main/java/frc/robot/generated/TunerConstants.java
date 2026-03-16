9caaf6e2fbd759df000340c9b7ac6a2e7945a6c7
Ace-h121 <chathaway121@gmail.com>
2026-03-16 12:43:53 -0400

    adding current limits and stuff

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -65,8 +65,10 @@ public class TunerConstants {
                     // Swerve azimuth does not require much torque output, so we can set a
                     // relatively low
                     // stator current limit to help avoid brownouts without impacting performance.
-                    .withSupplyCurrentLimit(Amps.of(60))
-                    .withSupplyCurrentLimitEnable(true));
+                    .withSupplyCurrentLimit(Amps.of(50))
+                    .withSupplyCurrentLimitEnable(true))
+             .withClosedLoopRamps(new ClosedLoopRampsConfigs()
+             .withVoltageClosedLoopRampPeriod(0.01));
                     
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
             .withCurrentLimits(
@@ -74,9 +76,11 @@ public class TunerConstants {
                             // Swerve azimuth does not require much torque output, so we can set a
                             // relatively low
                             // stator current limit to help avoid brownouts without impacting performance.
-                            .withStatorCurrentLimit(Amps.of(60))
+                            .withStatorCurrentLimit(Amps.of(40))
                             .withSupplyCurrentLimit(Amps.of(20))
-                            .withStatorCurrentLimitEnable(true));
+                            .withStatorCurrentLimitEnable(true)
+                            .withSupplyCurrentLimitEnable(true));
+                            
     private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
     // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
     private static final Pigeon2Configuration pigeonConfigs = null;
